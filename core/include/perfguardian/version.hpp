6c96f63138a8a5dd9126f7c79e9a566ff8bf86b5
Sambit Kumar Mohanty <sambitkumarmohanty25@gmail.com>
2026-06-13 08:30:57 +0530

    feat: add Windows build

--- core/include/perfguardian/version.hpp
+++ core/include/perfguardian/version.hpp
@@ -4,9 +4,9 @@
 namespace perfguardian {
 
 inline constexpr int version_major = 0;
-inline constexpr int version_minor = 1;
+inline constexpr int version_minor = 2;
 inline constexpr int version_patch = 0;
-inline constexpr const char* version_str = "0.1.0";
+inline constexpr const char* version_str = "0.2.0";
 
 inline std::string version_string() {
     return std::string("PerfGuardian ") + version_str;
